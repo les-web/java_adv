c9bbbf6a37b92c65859ced770420ff98714aaf4b
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-07-05 12:44:35 +0200

    password hashing added & revised

--- src/main/java/oop/controller/UserController.java
+++ src/main/java/oop/controller/UserController.java
@@ -15,25 +15,29 @@ import java.util.Set;
  *
  ******************************/
 public class UserController implements UserControllerTemplate {
-    @Override
-    public void registerUser(User user) {
-        // kodowanie hasła
+    private String passwordEncoder(String password) {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             // operacja szyfrowania zwraca tablicę liczb naturalnych
-            byte[] passwordHash = md.digest(user.getPassword().getBytes());
+            byte[] passwordHash = md.digest(password.getBytes());
             // zapisanie tablicy liczb w typie String
             String passwordHashTxt = "";
             for (byte digit : passwordHash) {
                 passwordHashTxt += digit;
             }
-            // aktualizacja hasła w modelu user
-            user.setPassword(passwordHashTxt);
-            users.add(user);
-            System.out.println("Dodano nowego użytkownika, jego email : " + user.getEmail());
+            return passwordHashTxt;
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
+            return null;
         }
+    }
+
+    @Override
+    public void registerUser(User user) {
+        // kodowanie hasła
+        user.setPassword(passwordEncoder(user.getPassword()));
+        users.add(user);
+        System.out.println("Dodano nowego użytkownika, jego email : " + user.getEmail());
     }
 
     @Override
