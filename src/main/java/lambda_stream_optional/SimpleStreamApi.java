e7932b708d9850c7e92ff21be9f6a8ab28aceb60
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-07-12 09:01:26 +0200

    stream exercises 2

--- src/main/java/lambda_stream_optional/SimpleStreamApi.java
+++ src/main/java/lambda_stream_optional/SimpleStreamApi.java
@@ -7,14 +7,14 @@ import java.util.UnknownFormatConversionException;
 import java.util.stream.Collectors;
 
 public class SimpleStreamApi {
-    List<Double> numbers = new ArrayList<>(Arrays.asList(13.11, 16.66, 7.0, 33.43, 9.999999));
+    List<Double> numbers = new ArrayList<>(Arrays.asList(13.11, 16.66, 7.0, 33.43, 9.999999, 1.0, -20.0));
 
     // metoda wypisujaca liczby jedna pod druga
     public void getAllNumbers() {
         numbers.stream().forEach(number -> System.out.println(number));
     }
 
-    // metoda wypisujaca wszystkie liczby oddzielone ';'
+    // metoda wypisujaca wszystkie liczby oddzielone wybranym separatorem
     public void getAllNumbersCSV(String separator) {
         System.out.println(numbers.stream().
                 map(number -> String.valueOf(number))
@@ -30,12 +30,14 @@ public class SimpleStreamApi {
             System.out.println("Błędny zakres precyzji przy zaokraglaniu");
         }
     }
-// metoda zwracajaca sume wszystkich liczb w liscie
 
+    // metoda zwracajaca sume wszystkich liczb w liscie
     public double getSumOfAllNumbers() {
         return numbers.stream().mapToDouble(value -> Double.valueOf(value)).sum();
     }
 
+
+    // metoda zwracajaca najmnieksza liczbe
     public double getMin() {
         if (numbers.stream().mapToDouble(value -> Double.valueOf(value)).min().isPresent()) {
             return numbers.stream()
@@ -49,6 +51,7 @@ public class SimpleStreamApi {
 
     }
 
+    // metoda zwracajaca najwieksza liczbe
     public double getMax() {
         if (numbers.stream().mapToDouble(value -> Double.valueOf(value)).max().isPresent()) {
             return numbers.stream().
@@ -61,7 +64,7 @@ public class SimpleStreamApi {
         }
     }
 
-    // srenia
+    // metoda zwracajaca srednia arytmetyczna
     public double getAvg() {
         if (numbers.stream().mapToDouble(value -> Double.valueOf(value)).average().isPresent()) {
             return numbers.stream().mapToDouble(value -> Double.valueOf(value)).average().getAsDouble();
