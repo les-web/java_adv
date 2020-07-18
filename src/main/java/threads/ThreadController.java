fae70d85ceb28be103fa0d10310d79e5ae3c0383
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-07-18 15:41:26 +0200

    bank accounts threads 2

--- src/main/java/threads/ThreadController.java
+++ src/main/java/threads/ThreadController.java
@@ -4,49 +4,50 @@ import java.sql.Time;
 import java.util.*;
 
 public class ThreadController {
-    //  List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
-    Deque<Integer> numbers = new ArrayDeque<>(Arrays.asList(1, 2, 3, 4, 5));
+    Deque<Integer> numbers = new ArrayDeque<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
-    public void printNumber(Thread thread) {
-        Thread thread = new Thread(new Runnable() {
+    // metoda implementująca wątek wypisujący wszystkie liczby
+    public void printNumbers(Thread thread){
+        // utworzenie klasy anonimowej
+        thread = new Thread(new Runnable() {
             @Override
             public void run() {
-
-                //           for (Integer number : numbers) {
-                while (!numbers.isEmpty())
+                long timeStart = System.currentTimeMillis();
+                while(!numbers.isEmpty()) {
                     try {
-                        Thread.currentThread().sleep(new Random().nextInt(6) * 1000);
-                        synchronized (numbers) {
-                            System.out.println("Watek " + Thread.currentThread().getName() + " - wartosc - " + numbers.removeFirst()
-                                    + "aktualna zawartosc " + numbers);
-                            System.out.println("jeszcze cos robie");
+//                        Thread.currentThread().sleep(new Random().nextInt(6) * 1000);     // uśpienie wąktu thread na 1s
+                        Thread.currentThread().sleep(1000);
+                        synchronized (numbers) {            // synchronizacja - sekcja krytyczna
+                            System.out.println("Wątek: " + Thread.currentThread().getName() +
+                                    " - wartość: " + numbers.removeFirst() +
+                                    " aktualna zawartość: " + numbers);
+//                            Thread.currentThread().sleep(new Random().nextInt(10) * 100);
+                            Thread.currentThread().sleep(1000);
+                            System.out.println("Jeszcze coś robię");
+//                            Thread.currentThread().sleep(new Random().nextInt(10) * 100);
+                            Thread.currentThread().sleep(1000);
+                            System.out.println("i jeszcze");
                         }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
-                    } catch (NoSuchElementException e) {
+                    } catch (NoSuchElementException e){
                         break;
                     }
+                }
+                System.out.println("Czas [s]: " + ((System.currentTimeMillis() / 1000) - (timeStart / 1000)));
             }
-
-
         });
-        thread.start();
-
-
-        public static void main (String[]args){
-            ThreadController tc = new ThreadController();
-            long timeStart = System.currentTimeMillis();
-            Thread th1 = null, th2 = null, th3 = null;
-            System.out.println("watek AAA " + Thread.currentThread().getName());
-            tc.printNumber(th1);
-            tc.printNumber(th2);
-            tc.printNumber(th3);
-
-//        System.out.println("watek  BBB    " + Thread.currentThread().getName());
-//        tc.printNumber(th1);
-//        tc.printNumber(th2);
-//        tc.printNumber(th3);
-            System.out.println("Czas trwania : " + (timeStart - System.currentTimeMillis()));
-        }
+        thread.start();         // uruchomienie wątku -> "wykonanie metody run()"
     }
+    public static void main(String[] args) throws InterruptedException {
+        ThreadController tc = new ThreadController();
+        long timeStart = System.currentTimeMillis();
+        Thread th1 = null, th2 = null, th3 = null;
+        System.out.println("Wątek: " + Thread.currentThread().getName());
+        tc.printNumbers(th1);
+        tc.printNumbers(th2);
+        tc.printNumbers(th3);
+        System.out.println("Wątek: " +Thread.currentThread().getName());
 
+    }
+}
--- src/main/java/threadsAccount/Main.java
+++ src/main/java/threadsAccount/Main.java
@@ -1,14 +1,14 @@
-package threadAccount;
+package threadsAccount;
 
 public class Main {
     public static void main(String[] args) {
-        // jeden rachunek i jedna karta
-        Account a1 = new Account("1111-2222-3333", 10000);
-   //     Client c1 = new Client(a1);
+        // jeden rachunek i dwie karty
+        Account a1 = new Account("0000-0000-0000-0000", 1000);
         Thread th1 = new Thread(new Client(a1));
         Thread th2 = new Thread(new Client(a1));
-
+        Thread th3 = new Thread(new Client(a1));
         th1.start();
         th2.start();
+        th3.start();
     }
 }
