c8bc44dc3f804479a5f2702350e7511ae976b256
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-07-18 14:22:18 +0200

    threads 2

--- src/main/java/threads/ThreadController.java
+++ src/main/java/threads/ThreadController.java
@@ -1,37 +1,52 @@
 package threads;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.sql.Time;
+import java.util.*;
 
 public class ThreadController {
-    List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 6, 3, 8, 9, 7));
+    //  List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    Deque<Integer> numbers = new ArrayDeque<>(Arrays.asList(1, 2, 3, 4, 5));
 
-    public void printNumber() {
+    public void printNumber(Thread thread) {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (Integer number : numbers) {
+
+                //           for (Integer number : numbers) {
+                while (!numbers.isEmpty())
                     try {
-                        Thread.currentThread().sleep(1000);
-                        System.out.println("Watek " + Thread.currentThread().getName());
-                        System.out.println(number);
+                        Thread.currentThread().sleep(new Random().nextInt(6) * 1000);
+                        synchronized (numbers) {
+                            System.out.println("Watek " + Thread.currentThread().getName() + " - wartosc - " + numbers.removeFirst()
+                                    + "aktualna zawartosc " + numbers);
+                            System.out.println("jeszcze cos robie");
+                        }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
+                    } catch (NoSuchElementException e) {
+                        break;
                     }
-                }
             }
 
 
         });
         thread.start();
+
+
+        public static void main (String[]args){
+            ThreadController tc = new ThreadController();
+            long timeStart = System.currentTimeMillis();
+            Thread th1 = null, th2 = null, th3 = null;
+            System.out.println("watek AAA " + Thread.currentThread().getName());
+            tc.printNumber(th1);
+            tc.printNumber(th2);
+            tc.printNumber(th3);
+
+//        System.out.println("watek  BBB    " + Thread.currentThread().getName());
+//        tc.printNumber(th1);
+//        tc.printNumber(th2);
+//        tc.printNumber(th3);
+            System.out.println("Czas trwania : " + (timeStart - System.currentTimeMillis()));
+        }
     }
 
-    public static void main(String[] args) {
-        ThreadController tc = new ThreadController();
-        System.out.println("watek  " + Thread.currentThread().getName());
-        tc.printNumber();
-        System.out.println("watek     " + Thread.currentThread().getName());
-
-    }
-}
