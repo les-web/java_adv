b2c8156f7b1eb01803fb63699abf21b64a74a275
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-07-19 11:01:40 +0200

    threads exceptions 1

--- src/main/java/threadsCoordination/threadsController.java
+++ src/main/java/threadsCoordination/threadsController.java
@@ -10,40 +10,23 @@ import java.util.List;
 // utworz dwa watki o nazwach
 // producer 1 - wypisuje na ekran zawartosc List<String>
 //  producer 2 - wypisuje na ekran zawartosc Array<Integer>
-//
+// producer  moze wypisywac dopiero gdy producer 1 skonczy dzialanie (join)
+// po zakonczeniu pracy obu watkow
+// program main niech wypisze KONIEC
 public class threadsController {
     List<String> names = new ArrayList<>(Arrays.asList("Adam", "Jan", "Anna", "Ola", "Iga"));
     int[] numbers = {22, 12, 33, 44, 7, 2, 99, 1, 1, 1, 1, 0, 0, 1};
-
+    Thread thread1;
+    Thread thread2;
 
     public void printNames() {
-        Thread thread = new Thread(new Runnable() {
+        thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
                 int i = 0;
                 while (i < names.size()) {
                     try {
-                        System.out.println(Thread.currentThread().getName() + "  " +    names.get(i));
-                        Thread.currentThread().sleep(1000);
-
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                i++;
-                }
-            }
-        });
-        thread.start();
-    }
-
-        public void printNumbers() {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int i = 0;
-                while (i < numbers.length) {
-                    try {
-                        System.out.println(Thread.currentThread().getName() + "  " +String.valueOf(numbers[i]));
+                        System.out.println(Thread.currentThread().getName() + " : " + names.get(i));
                         Thread.currentThread().sleep(1000);
 
                     } catch (InterruptedException e) {
@@ -52,12 +35,47 @@ public class threadsController {
                     i++;
                 }
             }
-        });
-        thread.start();
+        }, "Producer 1");
+        thread1.start();
     }
+
+    public void printNumbers() {
+        thread2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    thread1.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                ;
+                int i = 0;
+                while (i < numbers.length) {
+                    try {
+                        System.out.println(Thread.currentThread().getName() + " : " + String.valueOf(numbers[i]));
+                        Thread.currentThread().sleep(1000);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    i++;
+                }
+            }
+        }, "Producer 2");
+        thread2.start();
+    }
+
     public static void main(String[] args) {
-threadsController tc =new threadsController();
-tc.printNames();
-tc.printNumbers();
+        threadsController tc = new threadsController();
+        tc.printNames();
+        tc.printNumbers();
+        try {
+            tc.thread1.join();
+            tc.thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("KONIEC");
     }
 }
